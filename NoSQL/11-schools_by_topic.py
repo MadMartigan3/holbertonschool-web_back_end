c37522deecdf0ff577c7a704b8eb78bd1bbeb381
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-04-30 15:53:36 +0200

    projet 0 task 7-11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+""" 11-schools_by_topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ returns the list of school having a specific topic """
+    return mongo_collection.find({'topics': topic})
---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 9-insert_school.py """
+
+
+def insert_school(school_collection, **kwargs):
+    """function that inserts a new document in a collection based on kwargs"""
+    return school_collection.insert_one(kwargs).inserted_id
