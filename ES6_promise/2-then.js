37bb4620dce95e83fbaaff9145ddd3b1255fc0a6
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-05-16 12:02:00 +0200

    lint 2-3

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => (Error()))
+    .finally(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'Success',
-      };
-    })
-    .catch((error) => {
-      console.error(error);
-      return new Error();
     });
 }
