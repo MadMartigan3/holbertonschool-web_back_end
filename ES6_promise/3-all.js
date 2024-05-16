7aa3464cdaab82824100e4053f6e8ea9cce55977
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-05-16 10:39:06 +0200

    lint task 1/2/3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-      .then(([photoResponse, userResponse]) => {
-        console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-      })
-      .catch(error => {
-        console.error("Signup system offline");
-      });
-  }
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
