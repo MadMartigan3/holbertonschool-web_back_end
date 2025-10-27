14e4fc2579c17726946de34e8178dc07421766ce
MadMartigan3 <gabrielmilan3@hotmail.com>
2025-10-27 14:51:36 +0100

    projet Basic_authentication task 7 + pycodestle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,32 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
-    For now, this class is empty
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header
+        for Basic Authentication
+
+        Args:
+            authorization_header: the Authorization header string
+
+        Returns:
+            The Base64 part after 'Basic ' or None
+        """
+        # Si authorization_header est None, retourne None
+        if authorization_header is None:
+            return None
+
+        # Si authorization_header n'est pas une string, retourne None
+        if not isinstance(authorization_header, str):
+            return None
+
+        # Si authorization_header ne commence pas par 'Basic '
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        # Retourne la valeur après 'Basic ' (après l'espace)
+        return authorization_header[6:]
