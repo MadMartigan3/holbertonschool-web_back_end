ecc53e15e25292ade7593a1d6cfefd917753312a
MadMartigan3 <gabrielmilan3@hotmail.com>
2025-12-12 15:41:46 +0100

    projet user_authentication_service task 1

---
+++ user_authentication_service/db.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""DB module"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base, User
+
+
+class DB:
+    """DB class"""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a new user"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user with a keyword argument"""
+        try:
+            query = self._session.query(User).filter_by(**kwargs)
+            user = query.one()
+            return user
+        except NoResultFound:
+            raise
+        except Exception as e:
+            raise InvalidRequestError(str(e))
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user with a keyword argument"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
