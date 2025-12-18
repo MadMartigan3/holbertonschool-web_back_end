d821ce326647c18c63d02d370703efbdf360c824
MadMartigan3 <gabrielmilan3@hotmail.com>
2025-12-18 15:44:26 +0100

    task 7

---
+++ i18n/6-app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Flask app: locale priority (URL > user setting > headers > default).
+
+- Mock users via ?login_as=<id>
+- Store current user in flask.g.user
+- Locale selection order:
+  1) ?locale=fr|en
+  2) g.user["locale"] if supported
+  3) Accept-Language best match
+  4) Default ("en")
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """set Babel’s default locale ("en") and timezone ("UTC")"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},  # invalid/unsu
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel()
+
+
+def get_user():
+    """Get the user"""
+    uid = request.args.get("login_as", type=int)
+    return users.get(uid) if uid in users else None
+
+
+@app.before_request
+def before_request():
+    """Attach current user to flask.g for this request."""
+    g.user = get_user()
+
+
+def get_locale():
+    """Determine the best match with our supported languages"""
+    forced = request.args.get("locale", type=str)
+    if forced in app.config["LANGUAGES"]:
+        return forced
+
+    user = getattr(g, "user", None)
+    if user:
+        uloc = user.get("locale")
+        if uloc in app.config["LANGUAGES"]:
+            return uloc
+
+    match = request.accept_languages.best_match(app.config["LANGUAGES"])
+    if match:
+        return match
+
+    return app.config["BABEL_DEFAULT_LOCALE"]
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def index():
+    """This function handles GET requests to the root URL ('/')
+    and renders the main index template for the application."""
+    return render_template("6-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
