f678658331175e994155ffaedd5b2c3f37c727c4
MadMartigan3 <gabrielmilan3@hotmail.com>
2026-01-07 14:33:21 +0100

    projet MySQL_Advanced task 1-country_users.sql

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Script of creation of a table users if not exist with a country field
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
